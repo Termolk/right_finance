49f2f32953dccebc392103359759bf14dc79824d
Termolk <chizzplay@icloud.com>
2022-05-21 17:59:41 +0300

    Made activity with the ability to add or subtract money Data is transferred between activities using an intent

--- app/src/main/java/com/Termolk/rightfinance/MainActivity.java
+++ app/src/main/java/com/Termolk/rightfinance/MainActivity.java
@@ -1,14 +1,77 @@
 package com.Termolk.rightfinance;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewTotalMoney;
+    Button buttonSubtractMoney;
+    Button buttonAddMoney;
+    Intent intent;
+
+    final int REQUEST_CODE_CHANGE_MONEY = 1;
+
+    static int money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialViews();
+
+        intent = new Intent(this, MoneyTransactions.class);
+        listenButtons();
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_CHANGE_MONEY:
+                    money = data.getIntExtra("ChangedMoney", 0);
+                    textViewTotalMoney.setText(money + "");
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Неверный резалт", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void listenButtons() {
+        clickOnButtonAddMoney();
+        clickOnButtonSubtractMoney();
+    }
+
+    private void clickOnButtonAddMoney() {
+        buttonAddMoney.setOnClickListener(view -> {
+            intent.putExtra("action", "Добавить");
+            startActivityForResult(intent, REQUEST_CODE_CHANGE_MONEY);
+        });
+    }
+
+    private void clickOnButtonSubtractMoney() {
+        buttonSubtractMoney.setOnClickListener(view -> {
+            intent.putExtra("action", "Вычесть");
+            startActivityForResult(intent, REQUEST_CODE_CHANGE_MONEY);
+        });
+    }
+
+    private void initialViews() {
+        textViewTotalMoney = findViewById(R.id.textViewTotalMoney);
+        buttonSubtractMoney = findViewById(R.id.buttonSubtractMoney);
+        buttonAddMoney = findViewById(R.id.buttonAddMoney);
+    }
+
 }
---
+++ app/src/main/java/com/Termolk/rightfinance/MoneyTransactions.java
@@ -0,0 +1,62 @@
+package com.Termolk.rightfinance;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MoneyTransactions extends AppCompatActivity {
+    TextView textViewAction;
+    EditText editTextMoney;
+    Button buttonChange;
+
+    String action;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_transactions);
+
+        initialViews();
+
+        Bundle arguments = getIntent().getExtras();
+        if(arguments!=null){
+            action = arguments.get("action").toString();
+            textViewAction.setText(action);
+        }
+        clickOnButtonChange();
+
+    }
+
+    private void initialViews() {
+        textViewAction = findViewById(R.id.textViewAction);
+        editTextMoney = findViewById(R.id.editTextMoney);
+        buttonChange = findViewById(R.id.buttonChange);
+    }
+
+    private void clickOnButtonChange() {
+        buttonChange.setOnClickListener(view -> {
+            if (!editTextMoney.getText().toString().equals("")) {
+                Intent intent = new Intent();
+                if (action.equals("Добавить")) {
+                    intent.putExtra("ChangedMoney", MainActivity.money + Integer.parseInt(editTextMoney.getText().toString()));
+                } else if (action.equals("Вычесть")) {
+                    intent.putExtra("ChangedMoney", MainActivity.money - Integer.parseInt(editTextMoney.getText().toString()));
+                }
+                setResult(RESULT_OK, intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Вы не указали числовое значение", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
